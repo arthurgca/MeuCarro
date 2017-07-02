0d5d8f0afd932dd65b5e45aeb7a5dcc2e54bfe9e
Luna <mateuslunacc@gmail.com>
2017-07-02 12:35:26 -0300

    Adicionado filtro por ano ao gráfico do relatório de gastos.

--- app/src/main/java/projetoi/meucarro/ExpensesReportActivity.java
+++ app/src/main/java/projetoi/meucarro/ExpensesReportActivity.java
@@ -28,6 +28,7 @@ import java.text.DateFormatSymbols;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -41,8 +42,8 @@ public class ExpensesReportActivity extends AppCompatActivity {
     private GraphView expenseGraph;
     private ArrayList<BarGraphSeries<DataPoint>> series;
 
-    private String[] typesOfExpenses;
-    private boolean[] checkedItens;
+    private String[] typeOfExpenses;
+    private String[] yearsOfExpanses;
 
     private CarroUser currentCar;
     private DatabaseReference carrosUserRef;
@@ -59,32 +60,22 @@ public class ExpensesReportActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        ArrayList<String> typesOfExpensesList = new ArrayList<>();
-
-        typesOfExpensesList.add("Total");
-        typesOfExpensesList.addAll(Arrays.asList(getResources().getStringArray(R.array.adicionardialog_gastosarray)));
-        this.typesOfExpenses = typesOfExpensesList.toArray(new String[typesOfExpensesList.size()]);
-
-        this.checkedItens = new boolean[this.typesOfExpenses.length];
-
         carrosUserListener = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 if (dataSnapshot.child("lastCar").getValue() != null) {
                     lastCarId = dataSnapshot.child("lastCar").getValue().toString();
                     currentCar = dataSnapshot.child("carrosList").child(lastCarId).getValue(CarroUser.class);
+                    typeOfExpenses = generateTypeOfExpanses();
+                    yearsOfExpanses = generateYearsOfExpanses();
 
                     if (currentCar.listaGastos != null) {
                         initGraph();
-                    }
-
-                    else {
+                    } else {
                         Toast.makeText(ExpensesReportActivity.this, R.string.erro_nenhum_gasto,
                                 Toast.LENGTH_LONG).show();
                     }
-                }
-
-                else {
+                } else {
                     Toast.makeText(ExpensesReportActivity.this, R.string.msg_home_listacarrosvazia,
                             Toast.LENGTH_LONG).show();
                 }
@@ -98,9 +89,34 @@ public class ExpensesReportActivity extends AppCompatActivity {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         this.carrosUserRef = database.getReference().child("users").child(mAuth.getCurrentUser().getUid());
         this.carrosUserRef.addValueEventListener(carrosUserListener);
-
         this.expenseGraph = (GraphView) findViewById(R.id.expenseByDateGraphView);
+
     }
+
+    private String[] generateTypeOfExpanses() {
+        ArrayList<String> typesOfExpensesList = new ArrayList<>();
+
+        typesOfExpensesList.add("Total");
+        typesOfExpensesList.addAll(Arrays.asList(getResources().getStringArray(R.array.adicionardialog_gastosarray)));
+        return typesOfExpensesList.toArray(new String[typesOfExpensesList.size()]);
+    }
+
+    private String[] generateYearsOfExpanses() {
+        ArrayList<String> years = new ArrayList<>();
+
+        Calendar c = Calendar.getInstance();
+        int currentYear = c.get(Calendar.YEAR);
+        Date firstExpenseDate = this.currentCar.getListaGastos().get(0).getData();
+        c.setTime(firstExpenseDate);
+        int firstYear = c.get(Calendar.YEAR);
+
+        for (int y = firstYear; y <= currentYear; y++) {
+            years.add(Integer.toString(y));
+        }
+
+        return years.toArray(new String[years.size()]);
+    }
+
 
     private void setLegendRenderer() {
         LegendRenderer legendRenderer = new LegendRenderer(this.expenseGraph);
@@ -111,22 +127,24 @@ public class ExpensesReportActivity extends AppCompatActivity {
     }
 
     private void initGraph() {
+        int thisYear = Calendar.getInstance().get(Calendar.YEAR);
         this.setGridRenderer(0, 50);
         this.setStaticLabelsByMonth("pt", "BR");
         this.setLegendRenderer();
 
         this.initSeries();
-        this.loadSeries();
+        this.loadSeries(thisYear);
     }
 
     private void initSeries() {
         String[] seriesColors = getResources().getStringArray(R.array.color_expense);
         this.series = new ArrayList<>();
 
-        for (int serieIndex = 0; serieIndex < this.typesOfExpenses.length; serieIndex++) {
+        for (int serieIndex = 0; serieIndex < this.typeOfExpenses.length; serieIndex++) {
             BarGraphSeries serie = new BarGraphSeries<>(new DataPoint[]{});
 
-            serie.setTitle(this.typesOfExpenses[serieIndex].toString());
+            serie.setTitle(this.typeOfExpenses[serieIndex].toString());
+            serie.setSpacing(10);
             serie.setAnimated(true);
             serie.setDrawValuesOnTop(true);
             serie.setColor(Color.parseColor(seriesColors[serieIndex]));
@@ -136,9 +154,8 @@ public class ExpensesReportActivity extends AppCompatActivity {
         }
     }
 
-    private void loadSeries() {
-        int thisYear = Calendar.getInstance().get(Calendar.YEAR);
-        ArrayList<ArrayList<Gasto>> expensesByYear = this.currentCar.getExpensesByYear(thisYear);
+    private void loadSeries(int year) {
+        ArrayList<ArrayList<Gasto>> expensesByYear = this.currentCar.getExpensesByYear(year);
 
         for (int month = 0; month < expensesByYear.size(); month++) {
             ArrayList<Gasto> expensesMonth = expensesByYear.get(month);
@@ -147,8 +164,8 @@ public class ExpensesReportActivity extends AppCompatActivity {
             DataPoint dp = new DataPoint(month + 1, expensesSum);
             this.series.get(0).appendData(dp, true, MAX);
 
-            for (int i = 1; i < this.typesOfExpenses.length; i++) {
-                ArrayList<Gasto> expensesByType = this.currentCar.getExpensesByType(expensesMonth, this.typesOfExpenses[i]);
+            for (int i = 1; i < this.typeOfExpenses.length; i++) {
+                ArrayList<Gasto> expensesByType = this.currentCar.getExpensesByType(expensesMonth, this.typeOfExpenses[i]);
                 Double expensesTypeSum = this.currentCar.calculateExpensesSum(expensesByType);
 
                 DataPoint dpType = new DataPoint(month + 1, expensesTypeSum);
@@ -186,12 +203,36 @@ public class ExpensesReportActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == R.id.time_interval) {
+            AlertDialog.Builder mBuilder = new AlertDialog.Builder(ExpensesReportActivity.this);
+            mBuilder.setTitle(getResources().getString(R.string.choose_expense_year));
+
+            mBuilder.setSingleChoiceItems(this.yearsOfExpanses, -1, new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    expenseGraph.removeAllSeries();
+                    series.clear();
+                    initSeries();
+                    loadSeries(Integer.parseInt(yearsOfExpanses[which]));
+                }
+            });
+
+            mBuilder.setCancelable(false);
+            mBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    setLegendRenderer();
+                }
+
+            });
+            AlertDialog mDialog = mBuilder.create();
+            mDialog.show();
 
         } else if (id == R.id.choose_type_of_expense) {
             AlertDialog.Builder mBuilder = new AlertDialog.Builder(ExpensesReportActivity.this);
             mBuilder.setTitle(getResources().getString(R.string.choose_expense_type));
 
-            mBuilder.setSingleChoiceItems(this.typesOfExpenses, -1, new DialogInterface.OnClickListener() {
+            mBuilder.setSingleChoiceItems(this.typeOfExpenses, -1, new DialogInterface.OnClickListener() {
 
                 @Override
                 public void onClick(DialogInterface dialog, int which) {

6c23179020c59aaf7999b3b192c421580f540c8a
Arthur <arthur.azevedo@ccc.ufcg.edu.br>
2017-08-29 17:17:25 -0300

    Usando Nav Drawer atualizado.

--- app/src/main/java/projetoi/meucarro/MainActivity.java
+++ app/src/main/java/projetoi/meucarro/MainActivity.java
@@ -1,136 +1,40 @@
-/*
- * Copyright 2013 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package projetoi.meucarro;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Locale;
-
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v4.app.ActionBarDrawerToggle;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.Fragment;
-
+import android.support.v4.app.FragmentManager;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-/**
- * This example illustrates a common usage of the DrawerLayout widget
- * in the Android support library.
- * <p/>
- * <p>When a navigation (left) drawer is present, the host activity should detect presses of
- * the action bar's Up affordance as a signal to open and close the navigation drawer. The
- * ActionBarDrawerToggle facilitates this behavior.
- * Items within the drawer should fall into one of two categories:</p>
- * <p/>
- * <ul>
- * <li><strong>View switches</strong>. A view switch follows the same basic policies as
- * list or tab navigation in that a view switch does not create navigation history.
- * This pattern should only be used at the root activity of a task, leaving some form
- * of Up navigation active for activities further down the navigation hierarchy.</li>
- * <li><strong>Selective Up</strong>. The drawer allows the user to choose an alternate
- * parent for Up navigation. This allows a user to jump across an app's navigation
- * hierarchy at will. The application should treat this as it treats Up navigation from
- * a different task, replacing the current task stack using TaskStackBuilder or similar.
- * This is the only form of navigation drawer that should be used outside of the root
- * activity of a task.</li>
- * </ul>
- * <p/>
- * <p>Right side drawers should be used for actions, not navigation. This follows the pattern
- * established by the Action Bar that navigation should be to the left and actions to the right.
- * An action should be an operation performed on the current contents of the window,
- * for example enabling or disabling a data overlay on top of the current content.</p>
- */
-public class MainActivity extends AppCompatActivity {
-    private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
-    private ActionBarDrawerToggle mDrawerToggle;
-
-    private CharSequence mDrawerTitle;
-    private CharSequence mTitle;
-    private String[] arrayOpcoes;
-    private FragmentManager fragmentManager;
-    private ArrayList<String> listDrawer;
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         createHomeFragment();
 
-
-        mTitle = mDrawerTitle = getTitle();
-        arrayOpcoes = getResources().getStringArray(R.array.menu_array);
-
-        listDrawer = new ArrayList<>();
-        listDrawer.addAll(Arrays.asList(arrayOpcoes));
-
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
-
-        // set a custom shadow that overlays the main content when the drawer opens
-        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-        // set up the drawer's list view with items and click listener
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
-                R.layout.drawer_list_item, arrayOpcoes));
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-
-        // enable ActionBar app icon to behave as action to toggle nav drawer
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-
-        // ActionBarDrawerToggle ties together the the proper interactions
-        // between the sliding drawer and the action bar app icon
-        mDrawerToggle = new ActionBarDrawerToggle(
-                this,                  /* host Activity */
-                mDrawerLayout,         /* DrawerLayout object */
-                R.drawable.ic_drawer,  /* nav drawer image to replace 'Up' caret */
-                R.string.drawer_open,  /* "open  drawer" description for accessibility */
-                R.string.drawer_close  /* "close drawer" description for accessibility */
-                ) {
-            public void onDrawerClosed(View view) {
-                getSupportActionBar().setTitle(mTitle);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-
-            public void onDrawerOpened(View drawerView) {
-                getSupportActionBar().setTitle(mDrawerTitle);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-        };
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
-
-        if (savedInstanceState == null) {
-            selectItem(0);
-        }
     }
 
     private void createHomeFragment() {
@@ -142,67 +46,52 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        // Handle action buttons
-        switch(item.getItemId()) {
-
-        default:
-            return super.onOptionsItemSelected(item);
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
 
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            selectItem(position);
-        }
-    }
 
-    private void selectItem(int position) {
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
         Fragment fragment = null;
+        int id = item.getItemId();
 
-        switch (position) {
-            case 0:
-                fragment = new HomeFragment();
-                break;
-            case 1:
-                fragment = new ProfileFragment();
-                break;
-            case 2:
-                fragment = new TrocarCarroFragment();
-                break;
-            case 3:
-                fragment = new AdicionarCarroFragment();
-                break;
-            case 4:
-                fragment = new GasCalculatorFragment();
-                break;
-            case 5:
-                Intent ex = new Intent(MainActivity.this, ExpensesReportActivity.class);
-                ex.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-                startActivity(ex);
-                break;
-            case 6:
-                fragment = new CarroStatusFragment();
-                break;
-            case 7:
-                fragment = new OficinasFragment();
-                break;
-            case 8:
-                fragment = new CompararCarroFragment();
-                break;
-            case 9:
-                fragment = new MarketplaceFragment();
-                break;
+        if (id == R.id.nav_home) {
+            fragment = new HomeFragment();
+        } else if (id == R.id.nav_perfil) {
+            fragment = new ProfileFragment();
 
-            case 10:
-                Intent it = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(it);
-                FirebaseAuth.getInstance().signOut();
-                finish();
+        } else if (id == R.id.nav_trocarcarro) {
+            fragment = new TrocarCarroFragment();
+
+        } else if (id == R.id.nav_adicionar_carro) {
+            fragment = new AdicionarCarroFragment();
+        }else if (id == R.id.nav_calculadorabicombustiavel) {
+            fragment = new GasCalculatorFragment();
+        }else if (id == R.id.nav_relatoriogastos) {
+            Intent ex = new Intent(MainActivity.this, ExpensesReportActivity.class);
+            ex.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+            startActivity(ex);
+        }else if (id == R.id.nav_statuscarro) {
+            fragment = new CarroStatusFragment();
+        }else if (id == R.id.nav_oficinas) {
+            fragment = new OficinasFragment();
+        }else if (id == R.id.nav_compararcarros) {
+            fragment = new CompararCarroFragment();
+        } else if (id == R.id.nav_marketplace) {
+            fragment = new MarketplaceFragment();
+        } else if (id == R.id.nav_logout) {
+            Intent it = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(it);
+            FirebaseAuth.getInstance().signOut();
+            finish();
         }
 
         if (fragment != null) {
@@ -213,35 +102,9 @@ public class MainActivity extends AppCompatActivity {
 
 
         }
-        mDrawerList.setItemChecked(position, true);
-        mDrawerList.setSelection(position);
-        setTitle(listDrawer.get(position));
-        mDrawerLayout.closeDrawer(mDrawerList);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
-
-    @Override
-    public void setTitle(CharSequence title) {
-        mTitle = title;
-        getSupportActionBar().setTitle(mTitle);
-    }
-
-    /**
-     * When using the ActionBarDrawerToggle, you must call it during
-     * onPostCreate() and onConfigurationChanged()...
-     */
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        mDrawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        // Pass any configuration change to the drawer toggls
-        mDrawerToggle.onConfigurationChanged(newConfig);
-    }
-
 }
